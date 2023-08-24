b494cea7a3a9cbb344d92480c0b3f198d999aab4
r4z4 <r4z4aa@gmail.com>
2023-08-24 23:02:38 +0000

    the rest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum_server::run;
+use dotenvy::dotenv;
+// use dotenvy_macro::dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    // let database_uri = dotenv!("DATABASE_URL");
+    // This is e.g. for SQLite. So, here, uri = just the path to the DB file
+    // let connection = Database::connect("/Users/me/.myapp/data.db?mode=rwc").await?;
+    let pg_uri = "postgresql://postgres:postgres@db";
+    run(pg_uri).await
 }
+
+
+/*
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+*/
